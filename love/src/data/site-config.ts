12b2eb889fa1ba6be5f01febc3475f5c3980ed75
ehmusiccenter <ehmusiccenter@outlook.com>
2026-05-12 12:49:47 +0800

    Update site-config.ts

--- love/src/data/site-config.ts
+++ love/src/data/site-config.ts
@@ -9,8 +9,8 @@ export const siteConfig = {
   tagline: "讓指尖躍動的音符，奏出專屬於你的音樂旅程",
   phone: "(852) 6819 3515",
   phoneRaw: "+85268193515",
-  email: "ehmusiccenter@outlook.com",
-  address: "香港九龍何文田亞皆老街100號皆寧大廈地下A鋪",
+  email: "info@eh-music.com",
+  address: "香港九龍何文田亞皆老街102號皆寧大廈地下A鋪",
   whatsappUrl: "https://wa.me/85268193515",
   mapEmbedUrl:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.2!2d114.175!3d22.318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDE5JzA0LjgiTiAxMTTCsDEwJzMwLjAiRQ!5e0!3m2!1szh-TW!2shk!4v1234567890",
