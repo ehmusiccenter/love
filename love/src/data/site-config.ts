5aa4e820cd95b740353a1e5d5a9d8c265313bc26
ehmusiccenter <ehmusiccenter@outlook.com>
2026-05-19 12:50:27 +0800

    Update site-config.ts

--- love/src/data/site-config.ts
+++ love/src/data/site-config.ts
@@ -13,7 +13,7 @@ export const siteConfig = {
   address: "香港九龍何文田亞皆老街102號艷芳庭地下02鋪",
   whatsappUrl: "https://wa.me/85268193515",
   mapEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.2!2d114.175!3d22.318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDE5JzA0LjgiTiAxMTTCsDEwJzMwLjAiRQ!5e0!3m2!1szh-TW!2shk!4v1234567890",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d922.7228767919376!2d114.17332396950778!3d22.319942539355903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3404016f4ccd9fc5%3A0xe3466d34a9db66b!2z5Y2T6K296Z-z5qiC5Lit5b-DIEVIIE11c2ljIENlbnRlcg!5e0!3m2!1sen!2shk!4v1779166156491!5m2!1sen!2shk",
   transport: "旺角東站步行5分鐘",
   social: {
     facebook: "https://www.facebook.com/EHMusicCenter",
